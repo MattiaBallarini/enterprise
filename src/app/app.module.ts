2eef21f0c8a6b5a69cb8db2b9a50a827c9410796
MattiaBallarini <ballarini.mattia1990@gmail.com>
2023-06-07 23:22:32 +0200

    sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { AppRoutingModule } from './app-routing.module'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component'
 import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatIconModule } from '@angular/material/icon';
+import { MainPageComponent } from './main/main/main.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
+    MatListModule,
+    MatIconModule,
 
   ],
   providers: [],
---
+++ src/app/main/main/main.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainPageComponent {
+
+  opened: boolean = true
+
+
+  sidebarToggle = (): void => {
+
+    console.log(this.opened)
+    this.opened = !this.opened
+    console.log(this.opened)
+  }
+
+}
